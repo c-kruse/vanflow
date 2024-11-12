0b7ef7e6d84cb1371ee509384a586574b430c90d
Christian Kruse <christian@c-kruse.com>
2024-11-12 12:22:39 -0800

    slowboi tool to test a pokey vanflow client
    Signed-off-by: Christian Kruse <christian@c-kruse.com>

---
+++ go.mod
@@ -0,0 +1,11 @@
+module slowboi
+
+go 1.23.3
+
+require (
+	github.com/cenkalti/backoff/v4 v4.3.0
+	github.com/skupperproject/skupper v0.0.0-20241112085621-6bb42ceb20c3
+	golang.org/x/time v0.5.0
+)
+
+require github.com/Azure/go-amqp v1.0.5 // indirect
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/skupperproject/skupper/pkg/vanflow"
+	"github.com/skupperproject/skupper/pkg/vanflow/session"
+	"golang.org/x/time/rate"
+)
+
+var (
+	address   string
+	container string
+
+	limit int
+)
+
+func main() {
+	flag.StringVar(&address, "router-address", "amqp://127.0.0.1:5672", "AMQP endpoint")
+	flag.StringVar(&address, "router-container-id", "slowboi", "amqp container id")
+
+	flag.IntVar(&limit, "limit", 0, "Rate limit. AMQP Messages accepted per 60 seconds")
+	flag.Parse()
+
+	var (
+		limiter *rate.Limiter = rate.NewLimiter(rate.Inf, 1)
+	)
+
+	if limit > 0 {
+		mps := time.Minute / time.Duration(limit)
+		limiter = rate.NewLimiter(rate.Every(mps), 1)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	connectionFactory := session.NewContainerFactory(address,
+		session.ContainerConfig{
+			ContainerID: container,
+			BackOff:     backoff.NewConstantBackOff(time.Millisecond * 500),
+		})
+	ctr := connectionFactory.Create()
+	ctr.OnSessionError(func(err error) {
+		log.Printf("amqp session error: %v", err)
+	})
+	ctr.Start(ctx)
+
+	var wg sync.WaitGroup
+
+	listenAddress := func(address string) {
+		defer wg.Done()
+		messages := ctr.NewReceiver(address, session.ReceiverOptions{Credit: 10})
+		log.Printf("Started listening for vanflow messages: %q", address)
+		for {
+			if ctx.Err() != nil {
+				return
+			}
+			msg, err := messages.Next(ctx)
+			if err != nil {
+				log.Printf("vanflow source %q receive error: %s", address, err)
+				continue
+			}
+			limiter.Wait(ctx)
+			messages.Accept(ctx, msg)
+			vm, err := vanflow.Decode(msg)
+			log.Printf("Message %T: address: %s body: %v", vm, address, vm)
+
+		}
+
+	}
+	beacons := ctr.NewReceiver("mc/sfe.all", session.ReceiverOptions{Credit: 10})
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		addresses := make(map[string]struct{})
+		log.Println("Started listening for beacon messages")
+		for {
+			if ctx.Err() != nil {
+				return
+			}
+			msg, err := beacons.Next(ctx)
+			if err != nil {
+				log.Printf("Beacon receive error: %s", err)
+				continue
+			}
+			limiter.Wait(ctx)
+			beacons.Accept(ctx, msg)
+			b := vanflow.DecodeBeacon(msg)
+			log.Printf("BEACON: %v %s", b.Address, b.SourceType)
+			if _, ok := addresses[b.Address]; !ok {
+				addresses[b.Address] = struct{}{}
+				wg.Add(1)
+				go listenAddress(b.Address)
+				if b.SourceType == "ROUTER" {
+					wg.Add(1)
+					go listenAddress(b.Address + ".flows")
+				}
+			}
+		}
+	}()
+	wg.Wait()
+}
